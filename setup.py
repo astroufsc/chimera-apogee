bb527206e99311573c527dfa6a912092a191feda
William Schoenell <wschoenell@gmail.com>
2015-05-20 01:21:48 +0200

    Refactored from chimera_template to chimera_apogee. Removed example files.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name='chimera_template',
+    name='chimera_apogee',
     version='0.0.1',
-    packages=['chimera_template', 'chimera_template.instruments', 'chimera_template.controllers'],
-    url='http://github.com/astroufsc/chimera_template',
+    packages=['chimera_apogee', 'chimera_apogee.instruments'],
+    url='http://github.com/astroufsc/chimera_apogee',
     license='GPL v2',
     author='William Schoenell',
     author_email='william@iaa.es',
